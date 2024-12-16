05b87f3b0c68a064c47d371adb594d6ede4b796f
pstrikos <strikosps@gmail.com>
2024-12-16 08:51:21 +0100

    Update env.sh
    Signed-off-by: pstrikos <strikosps@gmail.com>

--- env.sh
+++ env.sh
@@ -6,3 +6,4 @@ export HDF5PATH=/usr/lib/x86_64-linux-gnu/hdf5/serial/lib
 export DRAMSIMPATH="$(pwd)/DRAMSim2"
 export LIBCONFIGPATH="$(pwd)/libconfig"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BOOKSIMPATH/src/
+export NO_OPT=1
